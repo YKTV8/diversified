5f6578accee2e42739b42ccdb7b0c20f69012fa6
Geoffrey Matthews <97923330+YKTV8@users.noreply.github.com>
2023-09-19 23:57:05 -0700

    09/19
    created main1.js for index only; code @ 165 for review shuffle created errors on all other html, undefined.

--- blue/assets/js/main1.js
+++ blue/assets/js/main1.js
@@ -157,16 +157,16 @@
 // Reviews 
 // **  rotateReviews is used on the index.html only for the cycling of the reviews given from google yelp or facebook. 
 // **  rotate is set to rotate thru every 10 secs without nav options. 
-// function rotateReviews(){
-// 	var firstSet = document.getElementById('features');
-// 	var secondSet = document.getElementById('hiddenfeatures');
-// 	var thirdSet = document.getElementById('hiddenfeatures1');
+function rotateReviews(){
+	var firstSet = document.getElementById('features');
+	var secondSet = document.getElementById('hiddenfeatures');
+	var thirdSet = document.getElementById('hiddenfeatures1');
 
-// 	var temp = features.innerHTML;
-// 	features.innerHTML = hiddenfeatures.innerHTML;
-// 	hiddenfeatures.innerHTML = hiddenfeatures1.innerHTML;
-// 	hiddenfeatures1.innerHTML = temp;
+	var temp = features.innerHTML;
+	features.innerHTML = hiddenfeatures.innerHTML;
+	hiddenfeatures.innerHTML = hiddenfeatures1.innerHTML;
+	hiddenfeatures1.innerHTML = temp;
 
-// }
-// setInterval(rotateReviews, 10000);
+}
+setInterval(rotateReviews, 10000);
 })(jQuery);
